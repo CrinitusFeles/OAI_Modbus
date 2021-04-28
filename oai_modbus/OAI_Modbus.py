2babe59f2efeb8ad74630fd3224b951bacc26a43
crinitusfeles <crinitusfeles@gmail.com>
2021-04-28 07:17:40 +0700

    fix: major version bump test
    BREAKING CHANGE: test
    
    footer

--- oai_modbus/OAI_Modbus.py
+++ oai_modbus/OAI_Modbus.py
@@ -2,7 +2,7 @@ from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 import serial.tools.list_ports
 import threading
 import time
-#v1.1.1 perf!!!!!!!
+#v1.1.1 perf!!!!!!!!
 
 class OAI_Modbus(ModbusClient):
     def __init__(self, **kwargs):
